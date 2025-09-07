7f5ffde83ac0128e55ed5d5f9a7faf5345448f2b
SawsanAllam <sawsanallam.332@gmail.com>
2025-09-07 21:09:06 +0300

    Add alx-react-app-new project as normal directory2

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -12,8 +12,7 @@ function MainContent() {
     >
       <h2 style={{ color: "#333" }}>Welcome to My Page</h2>
       <p style={{ color: "#555", lineHeight: "1.6" }}>
-        Here I share my favorite cities and why I love them. Traveling gives me
-        new perspectives and experiences that I cherish forever.
+       I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
         </>
