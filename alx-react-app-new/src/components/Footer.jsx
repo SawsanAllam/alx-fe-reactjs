dd305be9e72b928659b26b08f8632bfb03be207a
SawsanAllam <sawsanallam.332@gmail.com>
2025-09-07 21:04:39 +0300

    Add alx-react-app-new project as normal directory

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+
+function Footer() {
+    return (
+        <>
+         <footer
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        padding: "15px",
+        marginTop: "20px",
+      }}
+    >
+  <p>© 2023 City Lovers</p>
+</footer>
+        </>
+    );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,22 @@
+
+function MainContent() {
+    return (
+        <>
+      <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#eef6ff",
+        minHeight: "300px",
+        textAlign: "center",
+      }}
+    >
+      <h2 style={{ color: "#333" }}>Welcome to My Page</h2>
+      <p style={{ color: "#555", lineHeight: "1.6" }}>
+        Here I share my favorite cities and why I love them. Traveling gives me
+        new perspectives and experiences that I cherish forever.
+      </p>
+    </main>
+        </>
+    );
+}
+export default MainContent;
