526c7b41a0e827c5ff08332503b33ceb095a3688
SawsanAllam <sawsanallam.332@gmail.com>
2025-09-07 21:19:51 +0300

    counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import './App.css'
  import MainContent from './components/MainContent'
  import Footer from './components/Footer'
  import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
     <Header />
+    <Counter/>
     <MainContent />
      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     <Footer />
