d849574b7ba270b37c4cdf31a51e7fdc477a746c
SawsanAllam <sawsanallam.332@gmail.com>
2025-09-13 17:38:16 +0300

    zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+         <div style={{ padding: '20px' }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+    </>
+  )
+}
+
+export default App
